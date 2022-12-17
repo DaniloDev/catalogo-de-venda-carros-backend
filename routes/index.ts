e57bea66373331a83853b417673f093dccaf007a
Danilo Santos Nascimento <danisn2010@gmail.com>
2022-12-16 23:01:39 -0300

    feat: create admVeiculos rota

--- routes/index.ts
+++ routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from "express"
 
 const router: Router = Router()
 
-import veiculos from '../routes/veiculos';
+import veiculos from './veiculos';
+import admVeiculos from './admVeiculos';
 import series from '../routes/series';
 import users from '../routes/users';
 import auth from '../routes/auth';
 
 router.use('/veiculos', veiculos)
+router.use('/admVeiculos', admVeiculos)
 router.use('/series', series)
 router.use('/users', users)
 router.use('/auth', auth)
--- routes/veiculos.ts
+++ routes/veiculos.ts
@@ -1,42 +1,7 @@
 import { Router } from "express"
 import Veiculo from '../models/veiculo'
-import jwt from 'jsonwebtoken'
-const jwtSecret: any = process.env.JWTSECRET
 
 const router: Router = Router()
-
-router.use(async(req, res, next) => {
-    const token = req.headers['x-acess-token'] || req.body.token || req.query.token
-    if(token){
-        try{
-            const payload = jwt.verify(token, jwtSecret)
-            if(payload.roles.indexOf('restrito')>= 0){
-                next()
-            }else{
-                res.send({ success:false, errors: "wrong credentials"})
-            }
-           
-        }catch(e){
-            res.send({ success:false, errors:"wrong credentials"})
-        }
-    }else{
-        res.send({ success:false, errors: "wrong credentials"})
-    }
-})
-
-// CREATE
-router.post('/', async(req, res) => {
-    const veiculo = new Veiculo(req.body)
-    try{
-        await veiculo.save()
-        res.send(veiculo)
-    }catch(e){
-        res.send({
-            success: false,
-            errors: Object.keys(e.errors)
-        })
-      }
-})
 
 // READ
 router.get('/', async(req, res) => {
@@ -49,31 +14,6 @@ router.get('/:id', async(req, res) => {
     const veiculo = await Veiculo.findOne({ _id: req.params.id })
     res.send(veiculo)
 })
-
-// UPDATE
-router.put('/:id', async(req, res) => {
-    const veiculo =  await Veiculo.findOne({ _id: req.params.id })
-    veiculo.name = req.body.name
-    veiculo.status = req.body.status
-    try{
-        await veiculo.save()
-        res.send(veiculo)
-    }catch(e){
-        res.send({
-            success: false,
-            errors: Object.keys(e.errors)
-        }) 
-    }
-})
-
-// DELETE
-router.delete('/:id', async(req, res) => {
-    await Veiculo.deleteOne({ _id: req.params.id })
-    res.send({
-        success: true
-    })
-})
-
 
 
 export default router
